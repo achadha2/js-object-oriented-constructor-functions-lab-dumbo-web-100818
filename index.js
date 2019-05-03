061a24edeed4fd941c5b5ebf36c7fd5c68e0f4ba
Andy Chadha <andy.chadha2@gmail.com>
2019-05-03 23:11:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ function Scooter(year, color, model){
 }
 
 function Driver(name, age, experience){
-  this.year = year, 
-  this.color = color,
-  this.model = model
+  this.name = name, 
+  this.age = age,
+  this.experience = experience
 }
 
 function Scooter(address, city){
-  this.year = year, 
-  this.color = color,
-  this.model = model
+  this.address = address,
+  this.city = city
 }
